40f501943fb6259730b8e81c6c0a48a25f80ad2a
adsonb87 <adsonb87@gmail.com>
2019-05-31 16:58:58 -0300

    NodeJS

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -7,18 +7,17 @@
  	
  	app.post('/noticias/buscar', function(req,res){
  		var idNoticia = req.body;
- 		
  		var connetion = app.config.dbConnection();
  		var noticiasModel = app.app.models.noticiasModel;
 
- 		noticiasModel.getNoticia(idNoticia, connetion, function(error, result){
+ 		noticiasModel.getNoticia(idNoticia, connetion,function(error, result){
  			res.render("noticias/noticia", {noticia : result});
  		});
 
  	});
 
 
- 	app.get('/noticia', function(req,res){
+ 	/*app.get('/noticia', function(req,res){
  		
  		var connetion = app.config.dbConnection();
  		var noticiasModel = app.app.models.noticiasModel;
@@ -26,5 +25,5 @@
  		noticiasModel.getNoticia(connetion, function(error, result){
  			res.render("noticias/noticia", {noticia : result});
  		});
- 	});
+ 	});*/
  };
