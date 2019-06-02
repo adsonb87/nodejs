2799933b16895ad55e94cf800f86cf6ce1233031
adsonb87 <adsonb87@gmail.com>
2019-06-02 19:25:19 -0300

    NodeJS

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,26 @@
 module.exports = function(app){
 	app.get('/formulario_inclusao_noticia',function(req, res){
-		res.render("admin/form_add_noticia");
+		res.render("admin/form_add_noticia", {validacao : {}});
 	});
 
 	app.post('/noticias/salvar',function(req,res){
 		var noticia = req.body;
-	
+		
+		req.assert('titulo','Título é obrigatório').notEmpty();
+		req.assert('resumo','Resumo é obrigatório').notEmpty();
+		req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+		req.assert('autor','Autor é obrigatório').notEmpty();
+		req.assert('data_noticia','Data é obrigatória').notEmpty();
+		req.assert('noticia','Notícia é obrigatória').notEmpty();
+
+		var erros = req.validationErrors();
+
+		if(erros){
+			res.render("admin/form_add_noticia", {validacao : {erros}});
+			console.log(validacao);
+			return;
+		}
+
 		var connection = app.config.dbConnection();
 		var noticiasDao = new app.app.models.NoticiasDAO(connection);
 
